469a16484d9f94a4db37b224f47e0d37cf386980
Dennis Seller <dennis.seller@sogeti.nl>
2017-03-13 22:14:50 +0100

    Moved enum translations into a table.

--- src/utils.c
+++ src/utils.c
@@ -3,6 +3,48 @@
 
 #include <stdarg.h>
 
+const char *ast_translation[] = {
+    "TRANSLATION_UNIT",
+    "FUNCTION_DECLARATION",
+    "DECLARATION",
+    "DECLARATION_SPECIFIER_LIST",
+    "TYPE_SPECIFIER",
+    "DIRECT_DECLARATOR",
+    "FUNCTION_PROTOTYPE",
+    "DECLARATOR_LIST",
+    "STORAGE_CLASS_SPECIFIER",
+    "TYPE_QUALIFIER",
+    "INIT_DECLARATOR",
+    "UNARY_OPERATOR",
+    "CONDITIONAL",
+    "INCREMENT",
+    "ARE_EQUAL",
+    "ARE_NOT_EQUAL",
+    "ADD",
+    "SUBTRACT",
+    "DECREMENT",
+    "ARRAY_DECLARE",
+    "ARRAY_INDEX",
+    "CONSTANT",
+    "ASSIGNMENT",
+    "ASSIGNMENT_OPERATOR",
+    "BITSHIFT_LEFT",
+    "BITSHIFT_RIGHT",
+    "IS_GREATER_THAN",
+    "IS_LESS_THAN",
+    "IS_GREATER_THAN_OR_EQUAL",
+    "IS_LESS_THAN_OR_EQUAL",
+    "BITWISE_AND",
+    "BITWISE_OR",
+    "EXCLUSIVE_OR",
+    "LOGICAL_AND",
+    "LOGICAL_OR",
+    "MULTIPLY",
+    "DIVIDE",
+    "MODULO",
+    "SIZE_OF"
+};
+
 void ast_fatal(const char *reason, ...) {
     va_list ap;
     va_start(ap, reason);
@@ -42,43 +84,5 @@ void ast_dump_start(ast_node_t *root) {
 
 // TODO: put in table
 const char *ast_get_type_name(ast_node_type type) {
-    switch (type) {
-    case TRANSLATION_UNIT: return "TRANSLATION_UNIT";
-    case DECLARATION: return "DECLARATION";
-    case TYPE_SPECIFIER: return "TYPE_SPECIFIER";
-    case DIRECT_DECLARATOR: return "DIRECT_DECLARATOR";
-    case DECLARATOR_LIST: return "DECLARATOR_LIST";
-    case TYPE_QUALIFIER: return "TYPE_QUALIFIER";
-    case DECLARATION_SPECIFIER_LIST: return "DECLARATION_SPECIFIER_LIST";
-    case STORAGE_CLASS_SPECIFIER: return "STORAGE_CLASS_SPECIFIER";
-    case UNARY_OPERATOR: return "UNARY_OPERATOR";
-    case INIT_DECLARATOR: return "INIT_DECLARATOR";
-
-    case CONDITIONAL: return "CONDITIONAL";
-    case INCREMENT: return "INCREMENT";
-    case DECREMENT:return "DECREMENT";
-    case ARRAY_INDEX:return "ARRAY_INDEX";
-    case CONSTANT:return "CONSTANT";
-    case ARE_EQUAL: return "ARE_EQUAL";
-    case ARE_NOT_EQUAL: return "ARE_NOT_EQUAL";
-    case ADD: return "ADD";
-    case SUBTRACT: return "SUBTRACT";
-    case BITSHIFT_LEFT: return "BITSHIFT_LEFT";
-    case BITSHIFT_RIGHT: return "BITSHIFT_RIGHT";
-    case IS_GREATER_THAN: return "IS_GREATER_THAN";
-    case IS_LESS_THAN: return "IS_LESS_THAN";
-    case IS_GREATER_THAN_OR_EQUAL: return "IS_GREATER_THAN_OR_EQUAL";
-    case IS_LESS_THAN_OR_EQUAL: return "IS_LESS_THAN_OR_EQUAL";
-    case BITWISE_AND: return "BITWISE_AND";
-    case BITWISE_OR: return "BITWISE_OR";
-    case LOGICAL_AND: return "LOGICAL_AND";
-    case LOGICAL_OR: return "LOGICAL_OR";
-    case EXCLUSIVE_OR: return "EXCLUSIVE_OR";
-    case MULTIPLY: return "MULTIPLY";
-    case DIVIDE: return "DIVIDE";
-    case MODULO: return "MODULO";
-    case ARRAY_DECLARE: return "ARRAY_DECLARE";
-    case SIZE_OF: return "SIZE_OF";
-    default: return "Unknown";
-    }
+    return ast_translation[type];
 }
